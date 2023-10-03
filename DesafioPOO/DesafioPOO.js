b5ad3fd889932d9e679d0d7a6ca632889b062aa4
ricardobr2001 <128512881+ricardobr2001@users.noreply.github.com>
2023-10-03 02:14:38 -0300

    Add files via upload

---
+++ DesafioPOO/DesafioPOO.js
@@ -0,0 +1,95 @@
+//-----Class Cuarto de los juguetes (clase madre)
+
+//-----Consttructor
+
+//Soldadito de plomo
+//juguete Sorpresa
+//pelota de pong
+//mesa
+//marcador
+
+//-----Actualizar
+
+//si la pelota pasa ciera posicion en y se suma 1 punto
+//si la pelota choca con un jugador o las paredes rebota
+
+//-----Metodo dihujar
+
+//dibujar soladito
+//dibujar juguete
+//dibujar pelota 
+//dibujar mesa
+//dibujar marcador
+//------------------------------------------------------------------------
+
+//-----Subclase soladito de plomo
+
+//-----Constructor
+
+//posX = mouseX soldadito 
+//posY = y soldadito
+//imagen del soldadito
+
+//-----Metodo dibujar
+
+//insertar imagen del soldadito
+
+//------------------------------------------------------------------------
+
+//-----Subclase juguete sopresa
+
+//-----Constructor
+
+//posX = x1 juguete
+//posY = y1 juguete
+//imagen del jugguete sorpresa
+
+//-----Metodo dibujar
+
+//insertar imagen del juguete sorpresa
+
+//------------------------------------------------------------------------
+
+//-----Subclase pelota de pong
+
+//-----Constructor
+
+//posX = x2 pelota
+//posY = y2 pelota
+//velX = x5 velocidad y direccion de la pelota
+//velY = y5 velocidad y direccion de la pelota
+
+//-----Metodo dibujar
+
+//fill color
+//circle
+
+
+//------------------------------------------------------------------------
+
+//-----Subclase mesa (fondo del juego)
+
+//-----Constructor
+
+//posX = x3 mesa
+//posY = y3 mesa
+//imagen de la mesa
+
+//-----Metodo dibujar
+
+//insertar imagen de la mesa 
+
+//------------------------------------------------------------------------
+
+//-----Subclase marcador
+
+//-----Constructor
+
+//posX = x4 maracdor
+//posY = y4 marcodor
+
+
+//-----Metodo dibujar
+
+//texto 1 marcador del soldadito
+//texto 2 marcador del jueguete sopresa
